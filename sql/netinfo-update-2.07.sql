1b3695ba72fc5759dd9b5d3fbc346284e2fae541
root <root@dslater-lnx.igb.illinois.edu>
2015-12-02 07:37:19 -0600

    update error in sql update file

--- sql/netinfo-update-2.07.sql
+++ sql/netinfo-update-2.07.sql
@@ -1,16 +1,3 @@
-CREATE TABLE IF NOT EXISTS networks (
-  id INT NOT NULL AUTO_INCREMENT,
-  domain_id INT REFERNCES domains(id),
-  name VARCHAR(255),
-  network VARCHAR(255),
-  netmask VARCHAR(255),
-  vlan INT,
-  options TEXT,
-  enabled BOOLEAN DEFAULT 1,
-  last_updated TIMESTAMP,
-  PRIMARY KEY (id)
-);
-
 CREATE TABLE IF NOT EXISTS domains (
   id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(255),
@@ -23,6 +10,20 @@ CREATE TABLE IF NOT EXISTS domains (
   PRIMARY KEY(id)
 
 );
+
+CREATE TABLE IF NOT EXISTS networks (
+  id INT NOT NULL AUTO_INCREMENT,
+  domain_id INT REFERENCES domains(id),
+  name VARCHAR(255),
+  network VARCHAR(255),
+  netmask VARCHAR(255),
+  vlan INT,
+  options TEXT,
+  enabled BOOLEAN DEFAULT 1,
+  last_updated TIMESTAMP,
+  PRIMARY KEY (id)
+);
+
 
 ALTER TABLE namespace ADD network_id INT REFERENCES networks(id);
 
