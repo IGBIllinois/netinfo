10d437e28797c066b7b27cd0e17540fef36b6005
root <root@netsvc-new.igb.illinois.edu>
2018-07-17 14:08:43 -0500

    readded html files

---
+++ html/includes/header.inc.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<script src='includes/main.inc.js' type='text/javascript'></script>
+<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+<link rel="stylesheet" type="text/css"
+	href="vendor/components/bootstrap/css/bootstrap.min.css">
+<title><?php echo __TITLE__; ?></title>
+</head>
+
+<body>
+	<div class='navbar navbar-inverse'>
+		<div class='navbar-inner'>
+			<div class='container'>
+				<div class='span9 brand'>
+					<?php echo __TITLE__; ?>
+				</div>
+				<div class='span3 pull-right'>
+					<p class='navbar-text pull-right'>
+					<small>Version <?php echo __VERSION__; ?></small>
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class='container-fluid'>
+		<div class='row-fluid'>
+			<div class='span3 well'>
+				<div class='sidebar-nav'>
+					<ul class='nav nav-list'>
+					<li><a href='index.php'>All Networks</a></li>
+					<li class='divider'></li>
+					<?php echo $nav_html; ?>
+						<li class='divider'></li>
+						<li><a href='hardware.php'>Mac Addresses</a></li>
+						<li><a href='networks.php'>Networks</a></li>
+						<li><a href='domains.php'>Domains</a></li>
+						<li class='divider'></li>
+						<li><a href='logout.php'>Logout</a></li>
+					</ul>
+					
+				</div>
+			</div>
+			<div class='span9'>
---
+++ html/includes/session.inc.php
@@ -0,0 +1,35 @@
+<?php
+/////////////////////////////////////////
+//
+//	session.inc.php
+//
+//	Used to verify the user is
+//	logged in before proceeding
+//
+//	David Slater
+//	May 2009
+//
+//////////////////////////////////////////
+
+require_once 'includes/main.inc.php';
+require_once 'authenticate.inc.php';
+
+$session = new session(__SESSION_NAME__);
+if (time() > $session->get_var('timeout') + __SESSION_TIMEOUT__) {
+	unset($_POST);
+	header('Location: logout.php');
+
+}
+elseif (!$session->get_var('login')) {
+	unset($_POST);
+	header('Location:logout.php');	
+
+}
+
+else {
+	//Reset timeout
+	$session_vars = array('timeout'=>time());
+	$session->set_session($session_vars);	
+}
+
+?>
