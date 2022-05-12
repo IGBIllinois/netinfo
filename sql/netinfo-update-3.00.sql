bb9f8968c1d38aaa1020780a97f323b0b20bfbee
root <root@netsvc-new.igb.illinois.edu>
2022-05-12 14:42:56 -0500

    Fixed bug in location_latest view

--- sql/netinfo-update-3.00.sql
+++ sql/netinfo-update-3.00.sql
@@ -97,7 +97,7 @@ AS SELECT a.id,
 	switches.hostname as switch 
 	FROM locations a
 	LEFT JOIN switches ON switches.switch_id=a.switch_id
-	LEFT JOIN (SELECT m1.switch_id,m1.port,m1.mac,m1.date FROM macwatch m1 WHERE m1.date= (SELECT MAX(macwatch.date) FROM macwatch WHERE macwatch.mac=m1.mac )) AS b
+	LEFT JOIN (SELECT m1.switch_id,m1.port,m1.mac,m1.date FROM macwatch m1 WHERE m1.date= (SELECT MAX(macwatch.date) FROM macwatch WHERE macwatch.port=m1.port AND macwatch.switch_id=m1.switch_id )) AS b
 	ON (b.port=a.port AND b.switch_id=a.switch_id)	
 	ORDER BY room ASC;
 
--- sql/netinfo.sql
+++ sql/netinfo.sql
@@ -119,18 +119,17 @@ AS SELECT m1.switch_id AS switch_id,
         WHERE m1.date = (SELECT MAX(macwatch.date) FROM macwatch WHERE macwatch.mac = m1.mac)\p;
 
 CREATE ALGORITHM=MERGE DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `location_latest`
-AS SELECT a.id, 
-	a.switch_id, 
-	a.port, 
-	a.jack_number, 
-	a.room, 
-	a.building,
-	b.mac,
-	b.date AS last_seen,
-	switches.hostname as switch 
-	FROM locations a
-	LEFT JOIN switches ON switches.switch_id=a.switch_id
-	LEFT JOIN (SELECT m1.switch_id,m1.port,m1.mac,m1.date FROM macwatch m1 WHERE m1.date= (SELECT MAX(macwatch.date) FROM macwatch WHERE macwatch.mac=m1.mac )) AS b
-	ON (b.port=a.port AND b.switch_id=a.switch_id)	
-	ORDER BY room ASC\p;
-
+AS SELECT a.id,
+        a.switch_id,
+        a.port,
+        a.jack_number,
+        a.room,
+        a.building,
+        b.mac,
+        b.date AS last_seen,
+        switches.hostname as switch
+        FROM locations a
+        LEFT JOIN switches ON switches.switch_id=a.switch_id
+        LEFT JOIN (SELECT m1.switch_id,m1.port,m1.mac,m1.date FROM macwatch m1 WHERE m1.date= (SELECT MAX(macwatch.date) FROM macwatch WHERE macwatch.port=m1.port AND macwatch.switch_id=m1.switch_id )) AS b
+        ON (b.port=a.port AND b.switch_id=a.switch_id)
+        ORDER BY room ASC\p;
